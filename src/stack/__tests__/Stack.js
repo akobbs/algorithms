a06e483d75d4c49baef4e9b8c17c1a00044b19eb
akobbs <anton.kobbs@gmail.com>
2019-05-23 16:22:16 +0300

    Add tests for stack

---
+++ src/stack/__tests__/Stack.js
@@ -0,0 +1,84 @@
+import ArrayStack from '../ArrayStack'
+import ListStack from '../ListStack'
+
+const createArrayWithValues = numberOfValues =>
+  Array(numberOfValues)
+    .fill(0)
+    .map((value, index) => index)
+
+const runTests = stackCreator => {
+  test('push / pop', () => {
+    const stack = stackCreator()
+    const numberOfValues = 100
+    const values = createArrayWithValues(numberOfValues)
+
+    values.forEach(v => stack.push(v))
+
+    const resultValues = []
+    values.forEach(() => resultValues.push(stack.pop()))
+
+    expect(resultValues).toEqual(values.reverse())
+  })
+
+  test('pop for empty stack', () => {
+    const stack = stackCreator()
+    const popWithException = () => stack.pop()
+
+    expect(popWithException).toThrow(Error)
+
+    stack.push(1)
+    stack.pop()
+
+    expect(popWithException).toThrow(Error)
+  })
+
+  test('isEmpty', () => {
+    const stack = stackCreator()
+    const numberOfValues = 100
+    const values = createArrayWithValues(numberOfValues)
+
+    expect(stack.isEmpty()).toBe(true)
+
+    values.forEach(value => {
+      stack.push(value)
+      expect(stack.isEmpty()).toBe(false)
+    })
+
+    values.forEach(() => {
+      expect(stack.isEmpty()).toBe(false)
+      stack.pop()
+    })
+
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  test('size', () => {
+    const stack = stackCreator()
+    expect(stack.size()).toBe(0)
+
+    const numberOfValues = 100
+    const values = createArrayWithValues(numberOfValues)
+
+    values.forEach((value, index) => {
+      stack.push(value)
+      expect(stack.size()).toBe(index + 1)
+    })
+
+    values.forEach((value, index) => {
+      stack.pop()
+      expect(stack.size()).toBe(numberOfValues - index - 1)
+    })
+
+    const popWithException = () => stack.pop()
+    expect(popWithException).toThrow(Error)
+    expect(stack.size()).toBe(0)
+  })
+}
+
+describe('ArrayStack', () => {
+  runTests(() => new ArrayStack())
+})
+
+describe('ListStack', () => {
+  runTests(() => new ListStack())
+})
