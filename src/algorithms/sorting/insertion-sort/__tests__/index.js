2d7245c85fda09ce4279e105821670394113bb68
akobbs <anton.kobbs@gmail.com>
2019-06-02 14:47:39 +0300

    Add insertion sort implementation

---
+++ src/algorithms/sorting/insertion-sort/__tests__/index.js
@@ -0,0 +1,22 @@
+import insetionSort from '../index'
+
+const isSorted = array => {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+describe('insertion sort', () => {
+  test('sort for array in descending order', () => {
+    const size = 1000
+    const values = Array(size).map((value, index) => size - index)
+
+    insetionSort(values)
+
+    expect(isSorted(values)).toBe(true)
+  })
+})
---
+++ src/algorithms/sorting/insertion-sort/index.js
@@ -0,0 +1,13 @@
+// Worst case: O(n^2) time | O(1) space
+// Best case (sorted array): O(n) | O(1) space
+export default function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let value = array[i]
+    let j = i - 1
+    while (j >= 0 && value < array[j]) {
+      array[j] = array[j + 1]
+    }
+
+    array[j + 1] = value
+  }
+}
